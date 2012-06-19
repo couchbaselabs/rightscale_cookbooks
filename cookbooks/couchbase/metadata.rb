14f56aaa3108724a95cd37ff9b388e70881bb17a
Farshid Ghods <farshid.ghods@gmail.com>
2012-06-19 16:47:05 -0700

    fixed dependencies

--- cookbooks/couchbase/metadata.rb
+++ cookbooks/couchbase/metadata.rb
@@ -5,7 +5,5 @@ description      "Installs/Configures Couchbase Server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
-depends "db_mysql"
-depends "app_php"
 
-recipe "lamp::default", "Install Couchbase Server specific packages. Setup Couchbase Server specific default attributes"
+recipe "couchbase::default", "Install Couchbase Server specific packages. Setup Couchbase Server specific default attributes"
